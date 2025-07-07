5e55d6f43b7706e5f7f9950ecba33a1cd15b604b
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-07-07 20:25:52 -0300

    Atualização do deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,23 @@
-import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
-import { ProdService } from './data/services/services/prod.service';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { DevService } from './data/services/services/dev.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: ProdService,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      useClass: DevService,
       imports: [ConfigModule],
-}),
+    }),
     PostagemModule,
     TemaModule,
     AuthModule,
-    UsuarioModule,
   ],
   controllers: [AppController],
   providers: [],
